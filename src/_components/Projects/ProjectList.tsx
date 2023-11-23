fd4a2f2379d4f1ed68e6f670ae9a289371ab6d10
Matt Fergoda <mattfergoda@users.noreply.github.com>
2023-11-22 18:41:59 -0800

    Fix projects grid.

--- src/_components/Projects/ProjectList.tsx
+++ src/_components/Projects/ProjectList.tsx
@@ -17,8 +17,8 @@ interface projectListProps {
 
 export default function ProjectsList({ projects }: projectListProps) {
     return (
-        <div className="container m-auto min-h-screen flex items-center my-8">
-        <div className="max-w-[900px]">
+        <div className="flex justify-center my-8">
+        <div className="max-w-[900px] grid grid-cols-1 md:grid-cols-2 gap-8">
             {
                 projects.map(project => <ProjectCard project={project} />)
             }
